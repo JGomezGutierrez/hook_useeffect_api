f69c9ba1bb6f66e1576af5d786838453cf2b1af7
Juliet Gomez <yuligomg@gmail.com>
2024-05-30 20:03:46 -0500

    Exercises useEffect Random User

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { RandomUser } from './components/RandomUser';
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomUser/>
+    </div>
+  );
+}
+
+export default App;
